a2381f25640ba194eec7a5a3973f0817adb379a1
Hoang Nam <90971424+hoangnam-dev@users.noreply.github.com>
2026-08-14 21:06:29 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
+        for(int i = 0; i < strs.Length; i++) {
+            char[] chars = strs[i].ToCharArray();
+            Array.Sort(chars);
+            string sortS = new string(chars);
+            if(!groups.ContainsKey(sortS)){
+                groups.Add(sortS, new List<string>());
+            }
+            groups[sortS].Add(strs[i]);
+        }
+        return groups.Values.ToList();
+    }
+}
