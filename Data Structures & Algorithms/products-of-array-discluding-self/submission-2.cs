24816b469c7bd7bb824d8e6d0b4692c9e2ea682f
Hoang Nam <90971424+hoangnam-dev@users.noreply.github.com>
2026-08-19 09:17:56 +0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int len = nums.Length;
+        int[] result = new int[len];
+        int right = 1;
+        for(int i = 0; i < len; i++) {
+            result[i] = right;
+            right *= nums[i];
+        }
+
+        int left = 1;
+        for(int i = len - 1; i >= 0 ; i--) {
+            result[i] *= left;
+            left *= nums[i];
+        }
+
+        return result;
+    }
+}
