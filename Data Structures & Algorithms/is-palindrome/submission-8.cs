c1c62b7ec84b8da5626d2ab76a09aa75b9c1b09c
Hoang Nam <90971424+hoangnam-dev@users.noreply.github.com>
2026-08-19 13:33:39 +0700

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string regex = "[^a-zA-Z0-9]"; 
+        string str = Regex.Replace(s.ToLower(), regex, "");
+        StringBuilder sb = new StringBuilder(str);
+        sb.Replace(" ", "");
+        int i = 0;
+        int j = sb.Length - 1;
+        while(i < j) {
+            if(sb[i] != sb[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
