54acfa83317a79ede74c86e42fcd281d9f78b04f
Hoang Nam <90971424+hoangnam-dev@users.noreply.github.com>
2026-08-21 21:20:24 +0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if(nums.Length == 0) return 0;
+        if(nums.Length == 1) return 1;
+        Array.Sort(nums);
+        int longest = 0;
+        int count = 1;
+        for(int i = 0; i < nums.Length - 1; i++) {
+            if(nums[i] != nums[i + 1]) {
+                if(nums[i] + 1 == nums[i+1]) {
+                    count++;
+                }
+                else {
+                    if(count > longest){
+                        longest = count;
+                    }
+                    count = 1;
+                }
+            }
+        }
+        if(count > longest) longest = count;
+        return longest;
+    }
+}
