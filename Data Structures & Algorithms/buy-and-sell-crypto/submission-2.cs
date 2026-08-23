9ae2cdc632f1a977c36c43ae7e40f3148d6f9bc3
Hoang Nam <90971424+hoangnam-dev@users.noreply.github.com>
2026-08-23 20:48:47 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i <= prices.Length - 1; i++) {
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.Min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
